38e0d43211d19cb4411800269d2d963b34f204f5
Daniel Shafer <github@danielshafer.com>
2016-06-25 17:55:35 -0700

    add test block

--- chapter_2/simple_multiply.py
+++ chapter_2/simple_multiply.py
@@ -2,6 +2,9 @@ def multiply(num1, num2):
 	"""multiplies two numbers and returns the product"""
 	return num1 * num2
 
+if __name__=="__main__":
+    #write tests here
+
 print multiply(23, 43)
 print multiply.__doc__
 
